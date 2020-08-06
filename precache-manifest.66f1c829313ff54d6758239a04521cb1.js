3e61cb5787ec02df4df3fe876d06dea6cc1e1b92
amiirbazzii <amir.hossein.b.a1011@gmail.com>
2020-08-06 13:39:20 -0700

    Updates

---
+++ precache-manifest.66f1c829313ff54d6758239a04521cb1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "229dc3109300e1025f2def8dc8be0f93",
+    "url": "/Product-expiration-date/index.html"
+  },
+  {
+    "revision": "d5166aae272e4c8442bc",
+    "url": "/Product-expiration-date/static/css/2.e5e4f238.chunk.css"
+  },
+  {
+    "revision": "ab71d110930f851f165c",
+    "url": "/Product-expiration-date/static/css/main.60ca547b.chunk.css"
+  },
+  {
+    "revision": "d5166aae272e4c8442bc",
+    "url": "/Product-expiration-date/static/js/2.0ef4b493.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Product-expiration-date/static/js/2.0ef4b493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab71d110930f851f165c",
+    "url": "/Product-expiration-date/static/js/main.479462c2.chunk.js"
+  },
+  {
+    "revision": "9bc5ade404979a9fb39a",
+    "url": "/Product-expiration-date/static/js/runtime-main.c2a9bd04.js"
+  },
+  {
+    "revision": "1ab919e2495edbe303ebafc5834369d8",
+    "url": "/Product-expiration-date/static/media/Bg.1ab919e2.jpg"
+  },
+  {
+    "revision": "d0f5911d9a71bc7ec61eff8a4f25ea4e",
+    "url": "/Product-expiration-date/static/media/plus.d0f5911d.svg"
+  }
+]);
